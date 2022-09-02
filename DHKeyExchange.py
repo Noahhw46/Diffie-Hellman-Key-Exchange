55cbb089927f7ce5dbbc8eb50969a9c0613760ef
Noahhw46 <Noahhw46@gmail.com>
2022-09-02 15:24:03 -0400

    Cleaning up code, making a function to create keys

--- DHKeyExchange.py
+++ DHKeyExchange.py
@@ -1,20 +1,25 @@
 import random
 
-prime = int(input("enter a prime number, P: ")) #public modulus - agreed upon by both - should be prime
-generator = int(input("enter a generator, G: ")) #public number - agreed upon by both
+def generate_key(g, Ps, prime):
+    
+    key = ((pow(g, Ps)) % prime)
+    return key
 
-Person_1_Private_Secret = int(input("enter your lone secret: ")) #private random number, picked by you
+prime = int(input("Enter the prime number, P: ")) #public modulus - agreed upon by both - should be prime
+generator = int(input("Enter the generator, G: ")) #public number - agreed upon by both
+
+Person_1_Private_Secret = int(input("Enter your lone secret: ")) #private random number, picked by you
 Person_2_Private_Secret = random.randint(90, 180) #your friends private random number
 
-Person_1_Public_Key = ((pow(generator, Person_1_Private_Secret)) % prime) #public key established by you
+Person_1_Public_Key = generate_key(generator, Person_1_Private_Secret, prime) #public key established by you
 print("Your public key is: " + str(Person_1_Public_Key))
 
-Person_2_Public_Key = ((pow(generator, Person_2_Private_Secret)) % prime) #public key established by your friend
+Person_2_Public_Key = generate_key(generator, Person_2_Private_Secret, prime) #public key established by your friend
 print("Your friends public key is: " + str(Person_2_Public_Key)) 
 
-Shared_Secret_Person1 = ((pow(Person_2_Public_Key, Person_1_Private_Secret)) % prime) #secret shared by both, established by you
-Shared_Secret_Person2 = ((pow(Person_1_Public_Key, Person_2_Private_Secret)) % prime) #secret shared by both, established by your friend
+Shared_Secret_Person1 = generate_key(Person_2_Public_Key, Person_1_Private_Secret, prime) #secret shared by both, established by you
+Shared_Secret_Person2 = generate_key(Person_1_Public_Key, Person_2_Private_Secret, prime) #secret shared by both, established by your friend
 
 print("Secret key for A = ", str(Shared_Secret_Person1))
-print("Secret key for B = ", str(Shared_Secret_Person2))
+print("Secret key for B = ", str(Shared_Secret_Person2)) #This is for debugging purposes of course, in the real scenario nobody would know anybody else's secret key
 
